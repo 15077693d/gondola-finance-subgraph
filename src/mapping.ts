1c5335fced7724cd0f0d44f667c7ab9f49e6e14b
15077693d <oscaryiu.lapsang@gmail.com>
2021-10-07 06:48:42 +0800

    init subgraph codebase

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,69 +1,100 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  Contract,
+  Deposit as DepositContract,
+  EmergencyWithdraw as EmergencyWithdrawContract,
+  OwnershipTransferred as OwnershipTransferredContract,
+  Withdraw as WithdrawContract,
+} from "../generated/Contract/Contract";
+import {
   Deposit,
   EmergencyWithdraw,
   OwnershipTransferred,
-  Withdraw
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+  Withdraw,
+} from "../generated/schema";
 
-export function handleDeposit(event: Deposit): void {
+export function handleDeposit(event: DepositContract): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Deposit.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new Deposit(event.transaction.from.toHex());
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
   // Entity fields can be set based on event parameters
-  entity.user = event.params.user
-  entity.pid = event.params.pid
+  entity.user = event.params.user;
+  entity.pid = event.params.pid;
+  entity.amount = event.params.amount;
+  entity.timestamp = event.block.timestamp;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.BONUS_MULTIPLIER(...)
-  // - contract.endAt(...)
-  // - contract.getMultiplier(...)
-  // - contract.gondola(...)
-  // - contract.gondolaPerSec(...)
-  // - contract.migrator(...)
-  // - contract.owner(...)
-  // - contract.pendingGondola(...)
-  // - contract.poolInfo(...)
-  // - contract.poolLength(...)
-  // - contract.startAt(...)
-  // - contract.totalAllocPoint(...)
-  // - contract.userInfo(...)
+  entity.save();
 }
 
-export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
+export function handleEmergencyWithdraw(
+  event: EmergencyWithdrawContract
+): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = EmergencyWithdraw.load(event.transaction.from.toHex());
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new EmergencyWithdraw(event.transaction.from.toHex());
+  }
 
-export function handleWithdraw(event: Withdraw): void {}
+  // Entity fields can be set based on event parameters
+  entity.user = event.params.user;
+  entity.pid = event.params.pid;
+  entity.amount = event.params.amount;
+  entity.timestamp = event.block.timestamp;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredContract
+): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = OwnershipTransferred.load(event.transaction.from.toHex());
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new OwnershipTransferred(event.transaction.from.toHex());
+  }
+
+  // Entity fields can be set based on event parameters
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
+  entity.timestamp = event.block.timestamp;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+}
+
+export function handleWithdraw(event: WithdrawContract): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Withdraw.load(event.transaction.from.toHex());
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new Withdraw(event.transaction.from.toHex());
+  }
+
+  // Entity fields can be set based on event parameters
+  entity.user = event.params.user;
+  entity.pid = event.params.pid;
+  entity.amount = event.params.amount;
+  entity.timestamp = event.block.timestamp;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+}
